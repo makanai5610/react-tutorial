ff72a041f30b1f6820aa47246e8a7db9bd51dad2
makanai5610 <takumi.k.5610@gmail.com>
2019-10-01 00:38:04 +0900

    apply component

--- src/js/client.js
+++ src/js/client.js
@@ -1,17 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-    render() {
-        return (
-            <h1>It's {this.get_result(114514)}!</h1>
-        );
-    }
-
-    get_result(num) {
-        return 1 + num;
-    }
-}
+import Layout from "./components/Layout"
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
