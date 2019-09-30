334cd81512cf46e98c3dbdd838f841c97cf0f0b9
makanai5610 <takumi.k.5610@gmail.com>
2019-10-01 00:42:42 +0900

    add title component

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Title from "./Header/Title";
 
 export default class Header extends React.Component {
     render() {
         return (
             <div>
-                <header>Header</header>
+                <Title />
             </div>
         );
     }
