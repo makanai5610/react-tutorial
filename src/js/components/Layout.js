2c135411636253f82a897b23fe807022a189fcf9
makanai5610 <takumi.k.5610@gmail.com>
2019-10-01 01:13:21 +0900

    binding

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -7,14 +7,13 @@ export default class Layout extends React.Component {
         super();
         this.state = { title: "Welcome" };
     }
+    changeTitle(title) {
+        this.setState({ title });
+    }
     render() {
-        setTimeout(
-            () => { this.setState({ title: "Welcome Name!" }); },
-            2000
-        );
         return (
             <div>
-                <Header title={this.state.title} />
+                <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
                 <Footer />
             </div>
         );
