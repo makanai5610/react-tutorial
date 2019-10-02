9ed62e35ab15aec06ff56311a60409d982408015
makanai5610 <takumi.k.5610@gmail.com>
2019-10-02 16:04:10 +0900

    use navigater

--- router/src/js/pages/Layout.js
+++ router/src/js/pages/Layout.js
@@ -1,7 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-export default class Layout extends React.Component {
+class Layout extends React.Component {
+    navigate() {
+        console.log(this.props.history);
+        this.props.history.push("/");
+    }
     render() {
         return (
             <div>
@@ -9,7 +13,10 @@ export default class Layout extends React.Component {
                 {this.props.children}
                 <Link to="archives"><button class="btn btn-danger">archives</button></Link>
                 <Link to="settings"><button class="btn btn-success">settings</button></Link>
+                <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
             </div>
         );
     }
 }
+
+export default withRouter(Layout);
